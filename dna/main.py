90e075094575a02a38607437718865ae23abbe4c
Bogdan Melnik <ld@hackerdom.ru>
2014-04-03 00:00:46 +0600

    [dna] Use Counter instead defaultdict

--- dna/main.py
+++ dna/main.py
@@ -1,8 +1,4 @@
-from collections import defaultdict
-import sys
+from collections import Counter
 
-counts = defaultdict(lambda: 0)
-for c in open('rosalind_dna.txt').read()[:-1]:
-    counts[c] += 1
-for key in sorted(counts.keys()):
-    sys.stdout.write(str(counts[key]) + " ")
+counts = Counter(open('rosalind_dna.txt').read()[:-1])
+print(" ".join([str(counts[key]) for key in sorted(counts.keys())]))
